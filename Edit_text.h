1682f1a5079faaa841f471ca5b689f2a1b922275
Bytto <108215545+Bytto@users.noreply.github.com>
2023-01-11 19:01:12 +0800

    C/C++Header
    头文件

---
+++ Edit_text.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// Edit_text 对话框
+
+class Edit_text : public CDialogEx
+{
+	DECLARE_DYNAMIC(Edit_text)
+
+public:
+	Edit_text(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~Edit_text();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit1();
+	CString FILE_NAME;
+};
---
+++ JIEYA_FILENAME.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include "afxdialogex.h"
+
+
+// JIEYA_FILENAME 对话框
+
+class JIEYA_FILENAME : public CDialogEx
+{
+	DECLARE_DYNAMIC(JIEYA_FILENAME)
+
+public:
+	JIEYA_FILENAME(CWnd* pParent = nullptr);   // 标准构造函数
+	virtual ~JIEYA_FILENAME();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG2 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CString TEXT_NAME;
+};
